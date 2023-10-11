78754a4be31f99f30905a324f6920e6410ed8fca
bogdan <bogdan@tevvo.com>
2023-10-11 17:21:49 +0300

    refactors publisher

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -13,7 +13,10 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-    private String address;
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
 
     private Set<Book> books;
 
@@ -21,10 +24,14 @@ public class Publisher {
     public Publisher() {
     }
 
-    public Publisher(Long id, String name, String address) {
+    public Publisher(Long id, String name, String addressLine1, String city, String state, String zip, Set<Book> books) {
         this.id = id;
         this.name = name;
-        this.address = address;
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.books = books;
     }
 
     public Long getId() {
@@ -43,12 +50,36 @@ public class Publisher {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
+    public String getAddressLine1() {
+        return addressLine1;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
     }
 
     public Set<Book> getBooks() {
@@ -79,7 +110,10 @@ public class Publisher {
         return "Publisher{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
+                ", addressLine1='" + addressLine1 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
                 ", books=" + books +
                 '}';
     }
